2bc29ae012c4b490a1b1e378688c368fc44b0e4d
Safi <safiullahkakar2020@outlook.com>
2024-12-02 01:45:08 +0430

    Added day 1 solution code

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,18 +1,47 @@
+import kotlin.math.abs
+
+fun getListOfPairDistances(input: List<String>): Pair<ArrayList<Int>, ArrayList<Int>> {
+    val leftSideValues = arrayListOf<Int>()
+    val rightSideValues = arrayListOf<Int>()
+    input.forEach {
+        it.split(' ').apply {
+            leftSideValues.add(first().toInt())
+            rightSideValues.add(last().toInt())
+        }
+    }
+   return leftSideValues to rightSideValues
+}
+
+fun calculateTotalDistance(pairDistances: Pair<ArrayList<Int>, ArrayList<Int>>): Int {
+    val sortedLeftSideList = pairDistances.first.sorted()
+    val sortedRightSideList = pairDistances.second.sorted()
+    return sortedLeftSideList.zip(sortedRightSideList) { r, l -> abs(r - l) }.sum()
+}
+
+fun calculateSimilarityScores(pairDistances: Pair<ArrayList<Int>, ArrayList<Int>>): List<Int> {
+    val leftSideList = pairDistances.first
+    val rightSideList = pairDistances.second
+
+    val similarityDistances = leftSideList.map { l -> l * rightSideList.count { r -> r == l } }
+    return similarityDistances
+}
+
+
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        val listPair = getListOfPairDistances(input)
+        return calculateTotalDistance(listPair)
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        val listPair = getListOfPairDistances(input)
+        return calculateSimilarityScores(listPair).sum()
     }
 
-    // Test if implementation meets criteria from the description, like:
-    check(part1(listOf("test_input")) == 1)
-
-    // Or read a large test input from the `src/Day01_test.txt` file:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    check(part1(testInput) == 11)
+
+    check(part2(testInput) == 31)
 
     // Read the input from the `src/Day01.txt` file.
     val input = readInput("Day01")
